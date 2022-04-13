5a8b1402b2cf933a3c06c3dbd55199547c89475c
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-13 17:18:25 -0500

    hooked up search button and input submission event listeners and query the API for the coin index

--- assets/javascript/recentsearches.js
+++ assets/javascript/recentsearches.js
@@ -1,49 +1,34 @@
 // use jQuery 
-var coinsDropdown = $('#dropdownMenuList');
-var coinsDropdownContainer = document.querySelector('#dropdownMenuList');
+// var coinsDropdown = $('#dropdownMenuList');
 
-var coinSymbolKVP = [
-    {name:"Bitcoin", symbol: "BTC"},
-    {name:"Ethereum", symbol:  "ETH"},
-    {name:"Tether", symbol:  "USDT"},
-    {name:"ION", symbol:"ION"},
-    {name:"Binance Coin", symbol:  "BNB"	},
-    {name:"USD Coin", symbol:  "USDC"},
-    {name:"XRPV",symbol: "XRP"	},
-    {name:"Solana", symbol:  "SOL"	},
-    {name:"Cardano", symbol:  "ADA"},
-    {name:"Terra", symbol:  "LUNA"},
-    {name:"HEX", symbol:  "HEX"},
-    {name:"Avalanche", symbol:  "AVAX"	},
-    {name:"Dogecoin", symbol:  "DOGE"	},
-    {name:"Binance", symbol:  "BUSD"},	
-    {name:"Polkadot", symbol:  "DOT"},
-    {name:"Terra", symbol:  "UST"	},
-    {name:"SHIBA INU", symbol: "SHIB"},	
-    {name:"Wrapped", symbol:  "BiB"},
-    {name:"Polygon", symbol:  "MATIC"},	
-    {name:"Crypto.com coin", symbol:  "CRO"},
-    {name:"NEAR Protocol", symbol:  "NEAR"	},
-    {name:"Dai", symbol:"DAI"	},
-    {name:"Litecoin", symbol:  "LTC"},
-    {name:"Cosmos", symbol:  "ATOM"},
-    {name:"Chainlink", symbol:  "LINK"},	
-    {name:"Uniswap", symbol:  "UNI"} ];
 
-for (i = 0; i < coinSymbolKVP.length; i++) {
-    // dynamically populate dropdown list of coin items
-    var listEl = $(`<li class="dropdown-item" data-coin="${coinSymbolKVP[i].name}">${coinSymbolKVP[i].name}</li>`);
-    coinsDropdown.append(listEl);
-}
+// get city name from user form
+var userCoinInputEl = $('#coin_input');
+// hook into button it clicking when a keyup equals enter in the form field
+var searchButton = $('.search_btn');
 
-// add event listener to the 
-// coinsDropdownContainer.addEventListener("click", function(e) {
-//     // use event delegation to ensure clean event matches the link list items 
-//     if (e.target.matches('.dropdown-item')){
-//         // console.log(e.target.dataset.coin)
-//         storeCoinSearch(e.target.dataset.coin);
-//     }
-// });
+// event listeners for button click or search
+searchButton.on("click", function () {
+        // call coinSearch(userCoinInputEl.val());
+    });
+
+userCoinInputEl.keydown( e => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        // call coinSearch(userCoinInputEl.val());
+    } 
+});    
+
+var coinsDropdownContainer = document.querySelector('.past_search_container');
+
+// add event listener to the dropdown list items themselves
+coinsDropdownContainer.addEventListener("click", function(e) {
+    // use event delegation to ensure clean event matches the link list items 
+    if (e.target.matches('.dropdown-item')){
+        console.log(e.target.dataset.coin);
+        // storeCoinSearch(e.target.dataset.coin);
+    }
+});
 
 
 // LOCAL STORAGE
@@ -54,6 +39,7 @@ var userPastSearches = JSON.parse(localStorage.getItem("userPastSearches"));
 var searchesContainerEl = $(".past_search_container");
 
 // check past searches and render them if they exist
+storeCoinSearch();
 checkPastSearches();
 
 // checks local storage to see if there's some data to grab and display
@@ -64,7 +50,9 @@ function checkPastSearches() {
         renderPastSearches();
     }
     else {
-        var placeholderSearchItemEl = $(`<li class="list-group-item">Prior coin searches will appear here...</li>`);
+        // old
+        // var placeholderSearchItemEl = $(`<li class="list-group-item">Prior coin searches will appear here...</li>`);
+        var placeholderSearchItemEl = $(`<li class="dropdown-item" >Prior coin searches will appear here...</li>`)
         searchesContainerEl.append(placeholderSearchItemEl);
     }
 }
@@ -86,8 +74,9 @@ function renderPastSearches() {
         // declare the particular list item from the todos array
         var coin = userPastSearches[i].searchedCoin;
         // for that list item make the list item in the DOM
-
-        var searchItemEl = $(`<li class="list-group-item list-group-item-action list-group-item-dark text-center col-3" >${coin}</li>`);
+        // old
+        // var searchItemEl = $(`<li class="list-group-item list-group-item-action list-group-item-dark text-center col-3" >${coin}</li>`);
+        var searchItemEl = $(`<li class="dropdown-item"">${coin}</li>`);
         // populate its text as array value
         searchesContainerEl.append(searchItemEl);
     }
@@ -110,3 +99,39 @@ function storeCoinSearch(searchedCoinName) {
 // function fetchyBoi() {
 //     // Fetch go here
 // }; 
+
+
+
+// var coinSymbolKVP = [
+//     {name:"Bitcoin", symbol: "BTC"},
+//     {name:"Ethereum", symbol:  "ETH"},
+//     {name:"Tether", symbol:  "USDT"},
+//     {name:"ION", symbol:"ION"},
+//     {name:"Binance Coin", symbol:  "BNB"	},
+//     {name:"USD Coin", symbol:  "USDC"},
+//     {name:"XRPV",symbol: "XRP"	},
+//     {name:"Solana", symbol:  "SOL"	},
+//     {name:"Cardano", symbol:  "ADA"},
+//     {name:"Terra", symbol:  "LUNA"},
+//     {name:"HEX", symbol:  "HEX"},
+//     {name:"Avalanche", symbol:  "AVAX"	},
+//     {name:"Dogecoin", symbol:  "DOGE"	},
+//     {name:"Binance", symbol:  "BUSD"},	
+//     {name:"Polkadot", symbol:  "DOT"},
+//     {name:"Terra", symbol:  "UST"	},
+//     {name:"SHIBA INU", symbol: "SHIB"},	
+//     {name:"Wrapped", symbol:  "BiB"},
+//     {name:"Polygon", symbol:  "MATIC"},	
+//     {name:"Crypto.com coin", symbol:  "CRO"},
+//     {name:"NEAR Protocol", symbol:  "NEAR"	},
+//     {name:"Dai", symbol:"DAI"	},
+//     {name:"Litecoin", symbol:  "LTC"},
+//     {name:"Cosmos", symbol:  "ATOM"},
+//     {name:"Chainlink", symbol:  "LINK"},	
+//     {name:"Uniswap", symbol:  "UNI"} ];
+
+// for (i = 0; i < coinSymbolKVP.length; i++) {
+//     // dynamically populate dropdown list of coin items
+//     var listEl = $(`<li class="dropdown-item" data-coin="${coinSymbolKVP[i].name}">${coinSymbolKVP[i].name}</li>`);
+//     coinsDropdown.append(listEl);
+// }
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,33 +1,114 @@
 // initialize the elements that are going to contain the info from the API call
-var coinName = document.createElement('p');
-var coinPrice = document.createElement('p');
-var coinVolume = document.createElement('p');
-var supplyList = $('#supply_list');
-// var coinCirc = document.createElement('p');
-// var coinTotal = document.createElement('p');
-// var coinMax = document.createElement('p');
+let coinName = document.createElement('p');
+let coinPrice = document.createElement('p');
+let coinVolume = document.createElement('p');
+let supplyList = $('#supply_list');
+
+
+let messariURL = 'https://data.messari.io/api/v2/assets?limit=20';
+
+// var altMeURL = `https://api.alternative.me/v2/ticker/bitcoin/`;
+let altMeURL = 'https://api.alternative.me/v2/ticker/bitcoin/';
+let altMeAlsoURL = 'https://api.alternative.me/v2/listings/';
+
+// get user form
+var userCoinInputEl = $('#coin_input');
+// hook into button it clicking when a keyup equals enter in the form field
+var searchButton = $('.search-btn');
+
+// event listeners for button click or search
+searchButton.on("click", function () {
+        // call coinSearch(userCoinInputEl.val());
+        // console.log(userCoinInputEl.val())
+        var test = userCoinInputEl.val();
+        getMessariData(test);
+    });
+
+userCoinInputEl.keydown( e => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        var test = userCoinInputEl.val();
+        getMessariData(test);
+    } 
+});    
+
+var coinsDropdownContainer = document.querySelector('.past_search_container');
+
+// add event listener to the dropdown list items themselves
+coinsDropdownContainer.addEventListener("click", function(e) {
+    // use event delegation to ensure clean event matches the link list items 
+    if (e.target.matches('.dropdown-item')){
+        console.log(e.target.dataset.coin);
+        // storeCoinSearch(e.target.dataset.coin);
+    }
+});
+
+
+
+function getMessariData(userEntry) {
+    fetch(messariURL)
+    .then( response => response.json())
+    .then(listingsData => {
+        
+        console.log(listingsData.data);
+        var fullData = listingsData.data;
+        var index = getIndex(fullData, userEntry);
+        console.log(index);
+        
+    });
+}
+
+
+function getIndex(blob, userInput) {
+    var myIndex = blob.findIndex(function(blob) {
+        return blob.slug == userInput;          
+    });
+    
+    if (myIndex < 0) {
+        myIndex = blob.findIndex(function(blob) {
+        return blob.symbol == userInput;
+        }); 
+    }
+    // use RegEx to ensure strings are happily formatted 
+    // source: https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+
+    // ^ matches the beginning of the string.
+    // \w matches any word character.
+    // {1} takes only the first character.
+    // Thus, ^\w{1} matches the first letter of the word.
+    
+    // | works like the boolean OR. It matches the expression after and before the |.
+    // \s+ matches any amount of whitespace between the words (for example spaces, tabs, or line breaks).
+    userInput = userInput.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+    if (myIndex < 0) {
+        myIndex = blob.findIndex(function(blob) {
+        return blob.name == userInput;
+        }); 
+    }
+    return myIndex;
+}
+
 
 // call function that gets price/volume/coinname info
 getCoinStats();
 
 // define function that gets price/volume/coinname info
 function getCoinStats() {
-    fetch(`https://api.alternative.me/v2/ticker/bitcoin/`)
+    
+    fetch(altMeURL)
     .then(response => response.json())
     .then(myTickerData => {
         // extracts data of coin ID THEN you can ask for the subsequent properties
-        // NEXT STEP get whatever is searched or dropped down is the coin
         coinName.textContent = myTickerData.data[1].name; 
         // coinPrice.textContent = myTickerData.data[1].quotes.USD.price
         // toLocaleString adds commas for readability 
         coinPrice.textContent = "$" + (myTickerData.data[1].quotes.USD.price).toLocaleString("en-US");
-        // console.log(myTickerData.data[1].quotes.USD.volume_24h);
         coinVolume.textContent = "Volume traded in 24 hours: " + (myTickerData.data[1].quotes.USD.volume_24h).toLocaleString("en-US");
-
+        // append basic stats to container first
         supplyList.append(coinName);
         supplyList.append(coinPrice);
         supplyList.append(coinVolume);
-        
+        // append circulation content to container second
         var coinCirc = $(`<li class="list-group-item d-flex justify-content-between align-items-center">Circulating supply:<span class="badge bg-primary rounded-pill">${(myTickerData.data[1].circulating_supply).toLocaleString("en-US")}</span></li>`);
         var coinTotal = $(`<li class="list-group-item d-flex justify-content-between align-items-center">Total supply:<span class="badge bg-primary rounded-pill">${(myTickerData.data[1].total_supply).toLocaleString("en-US")}</span></li>`);
         var coinMax = $(`<li class="list-group-item d-flex justify-content-between align-items-center">Max supply:<span class="badge bg-primary rounded-pill">${(myTickerData.data[1].max_supply).toLocaleString("en-US")}</span></li>`);
@@ -36,10 +117,6 @@ function getCoinStats() {
         supplyList.append(coinMax);
     });
 }
-
-// document.body.appendChild(coinCirc);
-// document.body.appendChild(coinTotal);
-// document.body.appendChild(coinMax);
 
 // use jQuery 
 var geckoList = $('.list-group-numbered');
@@ -51,10 +128,8 @@ function getTrending() {
     .then(response => response.json())
     .then(myTrendingData => {
         var coinPopularLength = myTrendingData.coins.length;
-            console.log(coinPopularLength);
         for (var i = 0; i < coinPopularLength; i++) {
             var coinPopularName = myTrendingData.coins[i].item.name;
-            console.log(coinPopularName);
             // Top-7 trending coins on CoinGecko as searched by users in the last 24 hours (Ordered by most popular first)
             // call on the top 7 coins from the coingecko top 7 list include name and image
             var coinPopularLi = $(`<li class="list-group-item d-flex justify-content-between align-items-start"><div class="ms-2 me-auto"><div class="fw-bold">Subheading</div>${coinPopularName}</div><img src="${myTrendingData.coins[i].item.small}"></li>`);
